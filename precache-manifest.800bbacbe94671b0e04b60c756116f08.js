a8466081ffc64893b6a4e8212da94750d42b7d3a
Givemore <gtchiwanza@gmail.com>
2020-05-16 22:39:16 +0100

    Updates

---
+++ precache-manifest.800bbacbe94671b0e04b60c756116f08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd865c5229011a1678226262f3e647e",
+    "url": "/github-cards-app1/index.html"
+  },
+  {
+    "revision": "872b1c05c99a85450349",
+    "url": "/github-cards-app1/static/css/main.aef9ca69.chunk.css"
+  },
+  {
+    "revision": "e9bc871cb229f089eb45",
+    "url": "/github-cards-app1/static/js/2.accb5d83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-cards-app1/static/js/2.accb5d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872b1c05c99a85450349",
+    "url": "/github-cards-app1/static/js/main.3fd4c2ed.chunk.js"
+  },
+  {
+    "revision": "641676b9c74f823b77c4",
+    "url": "/github-cards-app1/static/js/runtime-main.2bc27fff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28664ae03e273a5fd5b729af84affa07.js"
+  "/github-cards-app1/precache-manifest.800bbacbe94671b0e04b60c756116f08.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-cards-app1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
