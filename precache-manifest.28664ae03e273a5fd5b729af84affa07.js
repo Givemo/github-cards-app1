c5ad62fe976ecb08a66e1a35643830c2fe3569a3
Givemore <gtchiwanza@gmail.com>
2020-05-16 22:18:44 +0100

    Updates

---
+++ precache-manifest.28664ae03e273a5fd5b729af84affa07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df46300d23a16923a25571b7d137db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6d408775d90179be6bd",
+    "url": "/static/css/main.aef9ca69.chunk.css"
+  },
+  {
+    "revision": "e9bc871cb229f089eb45",
+    "url": "/static/js/2.accb5d83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.accb5d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d408775d90179be6bd",
+    "url": "/static/js/main.b870d764.chunk.js"
+  },
+  {
+    "revision": "0a1cb3f9185a0fc7e7bb",
+    "url": "/static/js/runtime-main.7e5955b6.js"
+  }
+]);
